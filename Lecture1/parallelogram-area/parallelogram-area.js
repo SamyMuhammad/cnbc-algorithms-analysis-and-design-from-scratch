6a628b34fe6e802c4e5931f7447128af47075bf6
Samy Muhammad <samy.m.reda@gmail.com>
2023-09-18 23:43:35 +0300

    parallelogram area

---
+++ Lecture1/parallelogram-area/parallelogram-area.js
@@ -0,0 +1,14 @@
+/* 
+The Law: A=bh
+1. Read the base value
+2. Read the height value
+3. Multiply the base by the height
+4. Display the result
+*/
+
+function area(b, h) {
+    return b * h;
+}
+
+console.log(area(10, 15)); // 150
+console.log(area(12, 9)); // 108
